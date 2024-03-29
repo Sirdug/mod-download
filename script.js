4cd6b6ff64a4533043f441ce1fdd97baaa38e9cb
Aaron <80724707+Sirdug@users.noreply.github.com>
2024-03-28 20:12:14 -0400

    sdfsfdsf

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     // Replace 'modpack_download_link' with the actual link to your modpack file
     window.location.href = 'https://condemned-alexis-sirdug.koyeb.app/api/public/dl/MVrJfTUq/Horror-2.5.zip';
+    
 });
 
 document.getElementById('downloadBtn2').addEventListener('click', function() {
