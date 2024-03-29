fd1ac5c37bbab4c2dcedae81993f74b1234aaf4a
Aaron <80724707+Sirdug@users.noreply.github.com>
2024-03-29 13:29:09 -0700

    fsdfssfd

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     // Replace 'modpack_download_link' with the actual link to your modpack file
-    window.location.href = 'https://condemned-alexis-sirdug.koyeb.app/api/public/dl/fZOQlPbH/Peak.zip';
+    window.location.href = 'https://condemned-alexis-sirdug.koyeb.app/api/public/dl/ZN8vOy_H/Peak.zip';
     
 });
 
 document.getElementById('downloadBtn2').addEventListener('click', function() {
     // Replace 'modpack_download_link' with the actual link to your modpack file
-    window.location.href = 'https://condemned-alexis-sirdug.koyeb.app/api/public/dl/iaEAgjZt/Horror-2.5.zip';
+    window.location.href = 'https://condemned-alexis-sirdug.koyeb.app/api/public/dl/ZN8vOy_H/Peak.zip';
 });
